6697191ed29d93b291069b8f38aa20042ceb268b
kazuokaze <kazuokaze00@gmail.com>
2024-09-12 15:52:16 +0530

    Started easy array problems

--- leetcode-practice/array-practice-file.py
+++ leetcode-practice/array-practice-file.py
@@ -1,1 +1,20 @@
-print('Hey')
+
+
+## 66. Plus One
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0 
+
+        digits.insert(0, 1)
+        return digits
+            
+
+sl = Solution()
+digits = [9,9,9]
+print(sl.plusOne(digits))
